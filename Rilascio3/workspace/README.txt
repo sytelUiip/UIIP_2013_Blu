ed107736d3b9aaa0e6accec87debfbce58ea8531
Eugenio Stromei <eugenio.stromei@gmail.com>
2013-05-03 23:24:04 +0200

    Rilascio 3

--- Rilascio3/workspace/README.txt
+++
@@ -1,3 +0,0 @@
-Il workspace qui presente contiene l'ultima versione del nostro applicativo.
-
-In particolare, all'interno del progetto DAO, è stato inserito un package contenente i test in JUnit per verificare la correttezza delle funzionalità sviluppate.
--- Rilascio3/workspace/workspaceRilascio3/BLI/src/uiip/gestori/iface/GestoreLoginIface.java
+++
@@ -1,8 +0,0 @@
-package uiip.gestori.iface;
-
-import uiip.entity.Account;
-
-public interface GestoreLoginIface {
-	public boolean controllaAccount(String username, String password) throws Exception;
-	public Account getAccount(String user, String pass) throws Exception;
-}
